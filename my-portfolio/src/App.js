ed31f45ee64a06faaed471e246d3d4184e2b2642
Kendall Arceo <arceokendall@gmail.com>
2020-07-20 21:16:57 -0500

    restarting portfolio with material ui component library. Added components to ensure material ui is operating correctly for the interface.

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,62 @@
+//Main app with the landing page 
+
+import React, { useRef , useEffect} from "react";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import {blue, red,} from "@material-ui/core/colors";
+import "./App.css";
+
+
+function App (){
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main:blue[300],
+      },
+      secondary: {
+        main: red[900],
+      },
+      
+    },
+  });  
+  
+  return (
+  
+  <ThemeProvider theme = {theme}>
+
+
+    <React.Fragment>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path = {"/"}>
+              <Home components = {Home} />
+              
+            </Route>
+            
+            <Route exact path = {"/Portfolio"}>
+              <Portfolio components = {Portfolio} />
+              
+            </Route>
+
+            <Route exact path = {"/Contact"}>
+              <Contact components = {Contact} />
+            
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
+      
+      
+    </React.Fragment>
+  </ThemeProvider>
+
+  
+  );
+}
+
+export default App;
