74e4ed411428e80080b6c7a20ff05a445f08fc39
Kendall Arceo <arceokendall@gmail.com>
2020-08-09 22:06:53 -0500

    cards stacked in flexbox for porfolio page.

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -13,11 +13,6 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import Link from '@material-ui/core/Link';
 
 const drawerWidth = 240;
@@ -138,15 +133,15 @@ export default function PersistentDrawerLeft() {
 
           <div className={classes.sectionDesktop} >
             <IconButton  color="inherit" >
-              <Badge  color="secondary">
-              <Link href = "/" variant ="body2">
+              <Badge >
+              <Link href = "/" variant ="body2" color="inherit">
                 Home 
               </Link> 
               </Badge>
             </IconButton>
             <IconButton color ="inherit" >
               <Badge>
-                <Link href = "/Portfolio" variant ="body2"> 
+                <Link href = "/Portfolio" variant ="body2" color ="inherit"> 
                   Portfolio
                 </Link>
               </Badge>
@@ -182,22 +177,32 @@ export default function PersistentDrawerLeft() {
         </div>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+        <IconButton  color="inherit" >
+              <Badge >
+              <Link href = "/" variant ="body2" color="inherit">
+                Home 
+              </Link> 
+              </Badge>
+            </IconButton>
+            <IconButton color ="inherit" >
+              <Badge>
+                <Link href = "/Portfolio" variant ="body2" color ="inherit"> 
+                  Portfolio
+                </Link>
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+             
+              aria-haspopup="true"
+              //onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <Typography>Contact</Typography>
+            </IconButton>
         </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        
       </Drawer>
       
     </div>
--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -4,6 +4,10 @@ import Footer from '../components/Footer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import ProjectCard from '../components/ProjectCard';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
       
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(2),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -41,15 +45,77 @@ export default function Home() {
   const classes = useStyles();
 
   return (
+
     <div className = {classes.image}>
-      <Grid container component="main" className={classes.root}>
-        <CssBaseline />
+      <CssBaseline />
+      <Grid container component="main" className={classes.root} alignItems ="center">
+        
         <Header/>
 
+          <Container maxWidth="lg" >
           <main>
+            <Grid container direction="row" justify="center" alignItems="center" spacing ={2}>
+              <Grid item xs = {6}>
+                <Typography variant="h2" component="h1" color ="primary" className = {classes.paper}>
+                  Projects
+                </Typography>
+              </Grid>
+            </Grid>
             
+            <Grid container direction="row" justify="center" alignItems="center" spacing ={2}>
+            <Grid item xs = {10}>
+              <Typography variant="h4" className = {classes.paper} gutterBottom>
+              Some assignments and homework content throughout the course 
+              </Typography>  
+            </Grid>
+            </Grid>
+
+            <Grid container direction="row" justify="center" alignItems="center" spacing ={2}>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row" justify="center" alignItems="center" spacing ={2}>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+              <Grid item xs = {12} lg={4}>
+                
+                <ProjectCard/>
+                
+                
+              </Grid>
+            </Grid>
             
+          
           </main>
+        </Container>
+
         <Footer/>
       </Grid>
     </div>
