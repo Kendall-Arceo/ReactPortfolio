2c511bcf3da4bd1a7f7c6c30914ebb5bc302e106
Kendall Arceo <arceokendall@gmail.com>
2020-08-14 02:00:54 -0500

    added an app.get

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
+app.get('/', function (req, res) {
+    res.render('App', {});
+});
 var port = process.env.PORT || 3000
 app.listen(port)
 console.log('server started ' + port)
