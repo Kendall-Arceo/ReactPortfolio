88361d4e2ffba9b50b54818d49227448774e505c
Kendall Arceo <arceokendall@gmail.com>
2020-08-22 16:34:18 -0500

    added a * on the import?

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 
-import serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 serviceWorker.register();
 
