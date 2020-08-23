5055313e72a4e0f1776afb2b89a6065b3a62a8b2
Kendall Arceo <arceokendall@gmail.com>
2020-08-22 19:06:04 -0500

    putting it back.

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.use(appstart)
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
 
 
